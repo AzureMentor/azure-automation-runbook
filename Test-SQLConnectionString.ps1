dc3ac9d32a8be5022232abec85602e7a8b5b3822
AzureMentor <30055505+AzureMentor@users.noreply.github.com>
2020-05-06 00:49:14 -0400

    Add more test cases.

--- Test-SQLConnectionString.ps1
+++ Test-SQLConnectionString.ps1
@@ -1,6 +1,6 @@
 <# 
     .DESCRIPTION 
-    Test SQL Connection.
+    Test SQL Connection.  For Azure Automation Runbook.
 #> 
 
 function Test-SQLConnectionString{
@@ -23,3 +23,14 @@ function Test-SQLConnectionString{
         return $false;
     }
 }
+
+# Get value of ExistingVariable
+$result = Get-AutomationVariable -Name 'ExistingVariable'
+Write-Output $result
+
+# Change value of ExistingVariable using Set-AutomationVariable
+Set-AutomationVariable -Name 'ExistingVariable' -Value "new value"
+
+# ERROR: Get-AutomationVariable : Variables asset not found. To create this Variables asset, navigate to the Assets blade and create a Variables asset named: UnknownVariable.
+$result = Get-AutomationVariable -Name 'UnknownVariable'
+Write-Output $result
